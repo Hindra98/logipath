308080802ae1aa34cffb3c4e29285c081726c5ba
Vadiny <vadinyfotsing@gmail.com>
2026-03-30 11:20:18 +0100

    Analytics and Insights improve

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Analytics />
+      <SpeedInsights />
       <App />
     </BrowserRouter>
   </StrictMode>,
